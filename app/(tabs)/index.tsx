ae245ad2f4509c99d51a8c1d48ea8bfc86e44984
tigaweb <nka.webtech.dev@gmail.com>
2025-04-02 21:54:59 +0900

    画像表示の追加

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.continer}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image}/>
+      </View>
     </View>
   );
 }
@@ -26,5 +27,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff'
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   }
 })
